8cf9ad0d364feeceb398c8b49936be8bafe61b98
Marc Racicot <marc.racicot@vention.cc>
2022-12-06 18:31:11 -0500

    some proof of concept

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 
-import React, { Component } from 'react';
+import React, { useState,Component } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/home";
 import Network from "./pages/network";
-import RemoteSupport from "./pages/remoteSupport"
+import {RemoteSupport,RemoteSupportOverlay} from "./pages/remoteSupport"
 import './index.css'
 
 export default function App(){
+  const [remoteSupportVisible,setRemoteSupportVisible] = useState(false);
+  const [connected,setConnected] = useState(false);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="network" element={<Network />} />
-          <Route path="remoteSupport" element={<RemoteSupport zozo={1234}/>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="network" element={<Network />} />
+            <Route path="remoteSupport" element={<RemoteSupport setConnected={()=>setConnected(true)} setRemoteSupportVisible={setRemoteSupportVisible}/>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <RemoteSupportOverlay remoteSupportVisible={remoteSupportVisible} connected={connected}/>
+    </div>
   );
 }
 
--- src/pages/remoteSupport.jsx
+++ src/pages/remoteSupport.jsx
@@ -17,73 +17,103 @@ import {
 } from '@dytesdk/react-ui-kit';
 
 import {
-    BrowserRouter as Router,
-    Link,
-    Route,
-    Routes,
-    useParams,
-  } from "react-router-dom";
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Routes,
+  useParams,
+} from "react-router-dom";
 
-function MyMeeting() 
-{
-    const { meeting } = useDyteMeeting();
+function MyMeeting() {
+  const { meeting } = useDyteMeeting();
 
-    provideDyteDesignSystem(document.body, {
+  provideDyteDesignSystem(document.body, {
     // sets light background colors
     theme: 'light',
     colors: {
-        danger: '#ffac00',
-        brand: {
+      danger: '#ffac00',
+      brand: {
         300: '#00FFE1',
         400: '#00FFFF',
         500: '#00E1D4',
         600: '#007B74',
         700: '#00655F',
-        },
-        text: '#071428',
-        'text-on-brand': '#ffffff',
-        'video-bg': '#E5E7EB',
+      },
+      text: '#071428',
+      'text-on-brand': '#ffffff',
+      'video-bg': '#E5E7EB',
     },
     borderRadius: 'extra-rounded',
-    });
+  });
 
-    return (
-      <div>
-          <div style={{ height: '50px', width: '100%', padding: '10px', display: 'flex', flexDirection: 'column', justifyContent:'flex-start', alignContent: 'center', alignItems: 'center' }}>
-              This is the pendant v3 remote support page
-          </div>
-          <div style={{ height: '100%', width: '80%', padding: '10px', display: 'flex', flexDirection: 'column', justifyContent:'flex-start', alignContent: 'center', alignItems: 'center' }}>
-              <DyteMeeting mode="fill" meeting={meeting} style={{ height: '480px', padding: '10px', display: 'flex', flexDirection: 'column' }} />
-          </div>
+  return (
+    <div>
+      <div style={{ height: '50px', width: '100%', padding: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignContent: 'center', alignItems: 'center' }}>
+        This is the pendant v3 remote support page
       </div>
-    );
+      <div style={{ height: '100%', width: '80%', padding: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignContent: 'center', alignItems: 'center' }}>
+        <DyteMeeting mode="fill" meeting={meeting} style={{ height: '480px', padding: '10px', display: 'flex', flexDirection: 'column' }} />
+      </div>
+    </div>
+  );
 }
 
-const RemoteSupport = (props) => {
-    console.log("RemoteSupport()")
-    const [isOnline, setIsOnline] = useState(null); 
-    const [meeting, initMeeting] = useDyteClient();
-    useEffect(() => {
+const RemoteSupportOverlay = (props) => {
+  const [meeting, initMeeting] = useDyteClient();
+
+  // console.log('RemoteSupportOverlay:remoteSupportVisible=', props.remoteSupportVisible)
+  console.log(props.connected)
+  useEffect(() => {
+    if (props.connected)
+    {
       console.log("RemoteSupport.useEffect()")
-      if (!meeting)
-      { 
+      if (!meeting) {
         console.log("RemoteSupport.useEffect -> !meeting")
         initMeeting({
-          authToken: 'token',
-          roomName: 'roomname',
+          authToken: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMDI3MjEyLWUyZjAtNDEyMi1hNDQzLTUyMGNiOTAxOTdhOSIsImxvZ2dlZEluIjp0cnVlLCJpYXQiOjE2Njk3NDc3NDYsImV4cCI6MTY3ODM4Nzc0Nn0.tst_DpdjzB6KtsspYsGJ_hPy3BHeWode653G1qnj6WCJqU6XvfmYj1ekJYP-ko_HQLL67Bggc7XCXQcin4ucfbqwIi8FSnSEtt9BCRNMCdHZKfk_0vm_0N3th3LDX542dqB6ZljpIucI7QmX9JjAo1UfVAarq18WqHbPy4l5fuBC2jpDaDrEfJhMgRGeMHBnqxjShTYSd-VGEFmYchtrojQP7KdYZsQBuTVNhy1YI567RIArVsh8__50WuvVOqxmn1FlZJf69w-jl7xDbRjlig2UoyozZkNFzv-aghhBD2lrWc2TzIyeVxfW_vhFq_agQDj8aA2_UhgGHkra8rKMKg',
+          roomName: 'mlwzun-ytauij',
           defaults: {
             audio: false,
             video: false,
           },
         });
-      } 
-    },[meeting]);
+      }
+    }
+  }, [props.connected]);
+  return (
+    <div id="allo" style={{ visibility: (props.remoteSupportVisible) ? 'visible' : 'hidden', position: 'absolute', width: '50%', height: '100%', top: '100px' }}>
+      <DyteProvider value={meeting}>
+        <p>Allo this is abc</p>
+        <MyMeeting />
+      </DyteProvider>
+    </div>
+  );
 
-    return (
-        <DyteProvider value={meeting}>
-          <MyMeeting />
-        </DyteProvider>
-      );
+  // return  (
+  //   <div id="allo" style={{visibility:'visible', position:'absolute', width:'50%', height: '100%', top:'100px'}}>
+  //     <DyteProvider value={meeting}>
+  //       <MyMeeting />
+  //     </DyteProvider>
+  //   </div>
+  // );
 }
 
-export default RemoteSupport;
+
+const RemoteSupport = (props) => {
+  useEffect(() => {
+    if (props.setRemoteSupportVisible) {
+      props.setRemoteSupportVisible(true);
+      props.setConnected();
+    }
+    return () => {
+      // Anything in here is fired on component unmount.
+      props.setRemoteSupportVisible(false);
+    }
+  }, [])
+
+}
+
+export {
+  RemoteSupport,
+  RemoteSupportOverlay
+}
