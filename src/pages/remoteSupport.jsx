5513ad1d17ae1281dc4f228fca191fa48b8ca7e5
Marc Racicot <marc.racicot@vention.cc>
2022-12-07 10:58:02 -0500

    env

--- src/pages/remoteSupport.jsx
+++ src/pages/remoteSupport.jsx
@@ -70,8 +70,8 @@ const RemoteSupportOverlay = (props) => {
       if (!meeting) {
         console.log("RemoteSupport.useEffect -> !meeting")
         initMeeting({
-          authToken: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMDI3MjEyLWUyZjAtNDEyMi1hNDQzLTUyMGNiOTAxOTdhOSIsImxvZ2dlZEluIjp0cnVlLCJpYXQiOjE2Njk3NDc3NDYsImV4cCI6MTY3ODM4Nzc0Nn0.tst_DpdjzB6KtsspYsGJ_hPy3BHeWode653G1qnj6WCJqU6XvfmYj1ekJYP-ko_HQLL67Bggc7XCXQcin4ucfbqwIi8FSnSEtt9BCRNMCdHZKfk_0vm_0N3th3LDX542dqB6ZljpIucI7QmX9JjAo1UfVAarq18WqHbPy4l5fuBC2jpDaDrEfJhMgRGeMHBnqxjShTYSd-VGEFmYchtrojQP7KdYZsQBuTVNhy1YI567RIArVsh8__50WuvVOqxmn1FlZJf69w-jl7xDbRjlig2UoyozZkNFzv-aghhBD2lrWc2TzIyeVxfW_vhFq_agQDj8aA2_UhgGHkra8rKMKg',
-          roomName: 'mlwzun-ytauij',
+          authToken: process.env.REACT_APP_AUTHTOKEN,
+          roomName: process.env.REACT_APP_ROOMNAME,
           defaults: {
             audio: false,
             video: false,
@@ -83,7 +83,6 @@ const RemoteSupportOverlay = (props) => {
   return (
     <div id="allo" style={{ visibility: (props.remoteSupportVisible) ? 'visible' : 'hidden', position: 'absolute', width: '50%', height: '100%', top: '100px' }}>
       <DyteProvider value={meeting}>
-        <p>Allo this is abc</p>
         <MyMeeting />
       </DyteProvider>
     </div>
